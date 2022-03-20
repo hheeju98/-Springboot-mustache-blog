d3fd6614fdd67d5811ba9020bed49479dd9fd964
hheeju98 <hheeju98@naver.com>
2022-03-20 22:21:30 +0900

    1. 엔티티 생성 완료 및 UI 작업중

---
+++ src/main/java/site/metacoding/dbproject3/Dbproject3Application.java
@@ -0,0 +1,13 @@
+package site.metacoding.dbproject3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Dbproject3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Dbproject3Application.class, args);
+	}
+
+}
---
+++ src/test/java/site/metacoding/dbproject3/Dbproject3ApplicationTests.java
@@ -0,0 +1,13 @@
+package site.metacoding.dbproject3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Dbproject3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
